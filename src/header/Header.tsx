f93d7df12653e3e7dda6f4a5ca17d9ab7ad60d8c
eswaraprasad <esweraprasad@gmail.com>
2025-05-24 09:05:37 +0530

    changes

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <header className="bg-gray-800 text-white py-4 px-5 flex justify-between items-center ">
       <h1 className="text-2xl">My Application</h1>
-      <select name="" id="" className=" bg-gray-800 px-10">
+      <select name="" id="" className=" bg-gray-800 px-10" onChange={(e) => {}}>
         <option value="en">English</option>
         <option value="te">Telugu</option>
       </select>
